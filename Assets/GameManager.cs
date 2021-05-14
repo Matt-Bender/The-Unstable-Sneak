347ccc0161787a8dc77dc207109848470812c87e
husky339 <bendermaj@gmail.com>
2021-05-14 19:28:30 -0400

    Star pickup counted now tracked by gamemanager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private int starsPickedUp = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,15 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void IncreaseStarCount()
+    {
+        starsPickedUp++;
+        if(starsPickedUp >= 3)
+        {
+            Debug.Log("You Win");
+        }
     }
     public void RestartScene()
     {
--- Assets/Scripts/StarPickup.cs
+++ Assets/Scripts/StarPickup.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class StarPickup : MonoBehaviour
 {
     private Animator sAnim;
+    private GameManager gameManagerScript;
+
+    private bool pickedup = false;
     // Start is called before the first frame update
     void Start()
     {
         sAnim = GetComponent<Animator>();
+        gameManagerScript = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !pickedup)
         {
+            pickedup = true;
+            gameManagerScript.IncreaseStarCount();
             sAnim.SetTrigger("Death");
         }
     }
